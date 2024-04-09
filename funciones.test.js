bfb4a49da747fde87301965e342b03ffd2f8c592
Angelica <angelica.arias33@gmail.com>
2024-04-09 00:33:25 -0500

    [ADD] adding the firs function test

---
+++ funciones.test.js
@@ -0,0 +1,21 @@
+const assert = chai.assert;
+
+describe("Test de funciones", function () {
+  describe("Función interactuarCadenas", function () {
+    it('Debería devolver "+-0" para las cadenas "+-+" y "+--"', function () {
+      assert.strictEqual(interactuarCadenas("+-+", "+--"), "+-0");
+    });
+
+    it('Debería devolver "000000" para las cadenas "--++--" y "++--++"', function () {
+      assert.strictEqual(interactuarCadenas("--++--", "++--++"), "000000");
+    });
+
+    it('Debería devolver "-+-+-+" para las cadenas "-+-+-+" y "-+-+-+"', function () {
+      assert.strictEqual(interactuarCadenas("-+-+-+", "-+-+-+"), "-+-+-+");
+    });
+
+    it('Debería devolver "-+00" para las cadenas "-++-" y "-+-+"', function () {
+      assert.strictEqual(interactuarCadenas("-++-", "-+-+"), "-+00");
+    });
+  });
+});
