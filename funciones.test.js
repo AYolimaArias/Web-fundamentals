74c37e05b65621f50d4147463a9d0f25d9a67a86
Angelica <angelica.arias33@gmail.com>
2024-04-09 00:34:25 -0500

    [ADD] adding the second function test

--- funciones.test.js
+++ funciones.test.js
@@ -18,4 +18,26 @@ describe("Test de funciones", function () {
       assert.strictEqual(interactuarCadenas("-++-", "-+-+"), "-+00");
     });
   });
+
+  describe("Función generarApodo", function () {
+    it('debería devolver "Rob" para "Robert"', function () {
+      assert.equal(generarApodo("Robert"), "Rob");
+    });
+
+    it('debería devolver "Kim" para "Kimberly"', function () {
+      assert.equal(generarApodo("Kimberly"), "Kim");
+    });
+
+    it('debería devolver "Jean" para "Jeannie"', function () {
+      assert.equal(generarApodo("Jeannie"), "Jean");
+    });
+
+    it('debería devolver "Greg" para "Gregory"', function () {
+      assert.equal(generarApodo("Gregory"), "Greg");
+    });
+
+    it("debería lanzar un error para nombres con menos de 4 caracteres", function () {
+      assert.throws(() => generarApodo("Sam"), Error, "Nombre muy corto");
+    });
+  });
 });
